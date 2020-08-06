4925ef26f54de12b95cd382f3c8e60650dc84989
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-08-07 00:22:06 +0300

    uibuild

---
+++ build/precache-manifest.c39dd4c297adc7e3fca6775d4e936b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3664294d01a62ec040ccf51df984d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5efa20663fbf619959e9",
+    "url": "/static/css/main.dbc73bf7.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "5efa20663fbf619959e9",
+    "url": "/static/js/main.a72028f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

30741c30fc2c0f9617c6997a8856c8b89c8829e7
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-08-20 23:49:12 +0300

    use mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -15,45 +18,17 @@ app.use(cors())
 app.use(express.json()) 
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.get('/api', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const note = notes.find(note => note.id ===  Number(request.params.id))
-  note ? response.json(note) : response.status(404).end()
+  Note.findById(request.params.id).then(note => response.json(note))
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => res.json(notes))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -67,14 +42,15 @@ app.post('/api/notes', (request, response) => {
   
   if (!body.content) return response.status(400).json({ error: "content missing " })
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date()
-  }
+  })
 
-  notes.concat(note)
+  note.save().then(n => {
+    response.json(n)
+  })
 
   response.json(note)
 })

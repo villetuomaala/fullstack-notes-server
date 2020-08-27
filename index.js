dc5173997d9dfbb25c594ed8606e594770480a69
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-08-27 23:01:21 +0300

    Validations

--- index.js
+++ index.js
@@ -23,15 +23,18 @@ app.get('/api', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => note ? response.json(note) : response.status(404).end())
     .catch(error => next(error))
 })
 
+
 app.get('/api/notes', (req, res) => {
   Note.find({}).then(notes => res.json(notes))
 })
+
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
@@ -40,6 +43,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
+
 
 app.put('/api/notes/:id', (request, response, next) => {
   const body = request.body
@@ -56,39 +60,40 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
   
-  if (!body.content) return response.status(400).json({ error: "content missing " })
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date()
   })
 
-  note.save().then(n => {
-    response.json(n)
-  })
-
-  response.json(note)
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => response.json(savedAndFormattedNote))
+    .catch(error => next(error))
 })
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
+  if (error.name === 'CastError') return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === 'ValidationError') return response.status(400).send({ error: error.message })
 
   next(error)
 }
 app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

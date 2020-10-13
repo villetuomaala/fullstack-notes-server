5ceb3e2700b0f90df4dd336fc95ea2c835bd72ad
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-10-13 22:29:22 +0300

    4c

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes.map(note => note.toJSON()))
 })
 
@@ -18,14 +19,19 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important === body.important || false,
     date: new Date(),
+    user: user.id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote.id)
+  await user.save()
+
   response.json(savedNote.toJSON())
 })
 
